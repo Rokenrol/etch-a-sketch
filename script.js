2e16d6355528a520fb312952c56ac6f30c4bd5ec
Alek <kralj.komshiluka@gmail.com>
2023-09-28 14:00:38 +0200

    Create initial grid of 16 cells, make code more readable

--- script.js
+++ script.js
@@ -5,46 +5,57 @@ Goals:
 of squares in the grid
  - Limit the number of squares user can input to 100
 2. On mouse over the color of the squares should change
-  - Write a function that generates random number 0-255 to use it for random rgb colors
+  - Write a function that generates a random number from 0-255 and use it for random RGB colors
 3. Write a loop that creates divs and appends them to the container div
 */
 
-// Event listeners and loops for drawing cells
-
-let cellNumber = 0;
+// Select grid container and the button that makes the grid
 
 const container = document.getElementsByClassName("container")[0];
 const button = document.querySelector('button');
+
+// Create default grid of 16 x 16 cells
+
+createGrid(16);
+
+/* Event listener that listens for the button click,
+   on click clears the existing grid and creates a new grid */
+
 button.addEventListener('click', () => {
   removeNodes(container);
-  gridPrompt();
-  for (let i = 0; i < cellNumber; i++) {
-    for (let j = 0; j < cellNumber; j++) {
-    let div = document.createElement('div');
-    let cellSize = (container.offsetWidth / cellNumber) + "px";
-    div.style.width = cellSize;
-    div.style.height = cellSize;
-    div.classList.add('cell');
-    div.addEventListener('mouseenter', (e) => {
-      e.target.style.backgroundColor = `rgba(${randomNumber()}, ${randomNumber()}, ${randomNumber()}, ${cellDarken()} )`;
-    })
-    container.appendChild(div);
-    }
-  }
+  createGrid(gridPrompt());
 });
 
-// Prompt for getting the number of cells per grid
+// Prompt box for getting the number of cells per grid
 
 function gridPrompt() {
-  cellNumber = +prompt("Please enter the size of the grid");
-  if (cellNumber > 100 || isNaN(cellNumber)) {
+  cells = +prompt("Please enter the size of the grid");
+  if (cells > 100 || isNaN(cells)) {
     gridPrompt();
   } else {
-    return cellNumber;
+    return cells;
   }
 }
 
-// Generate random RGB value
+// Function that creates a new grid 
+
+function createGrid(cellNumber) {
+  for (let i = 0; i < cellNumber; i++) {
+    for (let j = 0; j < cellNumber; j++) {
+      let div = document.createElement('div');
+      let cellSize = (container.offsetWidth / cellNumber) + "px";
+      div.style.width = cellSize;
+      div.style.height = cellSize;
+      div.classList.add('cell');
+      div.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = `rgba(${randomNumber()}, ${randomNumber()}, ${randomNumber()}, ${cellDarken()})`;
+      });
+      container.appendChild(div);
+    }
+  }
+}
+
+// Generate a random RGB value
 
 function randomNumber() {
   let randomRGBNumber = Math.floor(Math.random() * 256);
@@ -59,15 +70,14 @@ function removeNodes(parent) {
   }
 }
 
-// Function that changes opacity in RGBa
+// Function that changes the opacity (alpha channel in RGBa)
 
 let cellOpacity = 0.1;
 
-  function cellDarken() {
-    cellOpacity += 0.1;
-    if (cellOpacity > 1.0) {
-      cellOpacity = 0.1;
-    }
-    console.log(cellOpacity);
-    return cellOpacity;
+function cellDarken() {
+  cellOpacity += 0.1;
+  if (cellOpacity > 1.0) {
+    cellOpacity = 0.1;
   }
+  return cellOpacity;
+}
